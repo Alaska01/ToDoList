d22e9fb39a5f4549c5d43dae240daf87557c3439
Aye Daniel A <aadaniel108@gmail.com>
2021-08-13 21:48:30 +0100

    configure linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: {
-    index: path.resolve(__dirname, "./src/index.js"),
+    index: path.resolve(__dirname, './src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
   },
   plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
