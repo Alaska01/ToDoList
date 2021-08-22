43cf11de5e17289b25c290aabbb28ee1c58c9da9
Aye Daniel A <aadaniel108@gmail.com>
2021-08-22 20:04:29 +0100

    add test to project

--- src/modules/factories.js
+++ src/modules/factories.js
@@ -3,3 +3,4 @@ export const projectFactory = (name) => ({ id: Date.now().toString(), name, task
 export const toDoListFactory = (title, description, dueDate, prior) => ({
   id: Date.now().toString(), title, description, dueDate, prior,
 });
+
